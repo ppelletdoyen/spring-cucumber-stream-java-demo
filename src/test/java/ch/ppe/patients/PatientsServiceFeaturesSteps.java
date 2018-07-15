e4300aa3594930a3410c23d956d8333207123fa0
Pascal PELLET-DOYEN <p.pelletdoyen@gmail.com>
2018-07-15 17:56:24 +0200

    Correct indentation ...

--- src/test/java/ch/ppe/patients/PatientsServiceFeaturesSteps.java
+++ src/test/java/ch/ppe/patients/PatientsServiceFeaturesSteps.java
@@ -12,28 +12,28 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-@ContextConfiguration(classes= PatientsApplication.class)
+@ContextConfiguration(classes = PatientsApplication.class)
 public class PatientsServiceFeaturesSteps {
-	
+
 	@Autowired
 	private PatientsService service;
-	
+
 	private boolean success;
-	
-    @Given("^patients service exists$")
-    public void patients_service_exists() throws Throwable {
-        service.toString();
-    }
-    
-    @When("^I call patients service$")
-    public void i_call_patients_service() throws Throwable {
-        List<Patient> patients = service.list();
-        success = patients != null;
-    }
-    
-    @Then("^it should have been a success$")
-    public void it_should_have_been_a_success() throws Throwable {
-        Assert.assertTrue(success);
-    }
+
+	@Given("^patients service exists$")
+	public void patients_service_exists() throws Throwable {
+		service.toString();
+	}
+
+	@When("^I call patients service$")
+	public void i_call_patients_service() throws Throwable {
+		List<Patient> patients = service.list();
+		success = patients != null;
+	}
+
+	@Then("^it should have been a success$")
+	public void it_should_have_been_a_success() throws Throwable {
+		Assert.assertTrue(success);
+	}
 
 }
